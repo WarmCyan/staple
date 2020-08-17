dc0aff6c7948dca4e5faadff6696347a1f1036c3
Nathan Martindale <nathanamartindale@gmail.com>
2020-08-16 21:10:51 -0500

    Add fancy propagation functions

--- staple/graph.py
+++ staple/graph.py
@@ -6,8 +6,29 @@ class Graph:
         self.nodes = []
 
     def apply_plan(self, plan):
+        """ Adds all of the planned activity times to each relevant node's new_time """
         activities = plan.calculate_activity_totals()
         for activity in activities:
             for node in self.nodes:
                 if node == activity.node:
-                    node.time += activity.time_total
+                    node.new_time += activity.time_total
+
+    # def find_leaves(self):
+    #     leaves = []
+    #     for node in self.nodes:
+    #         if len(nodes.children) == 0:
+    #             leaves.append(children)
+
+    def resolve_activations(self):
+        """ Run activations to propagate leaf new times up all the way throughout graph. """
+        for node in self.nodes:
+            node.activation_applied = False
+
+        for node in self.nodes:
+            node.activate()
+
+        # TODO: technically can probably go in loop above?
+        for node in self.nodes:
+            node.time += node.new_time
+            node.new_time = 0
+                    
--- staple/node.py
+++ staple/node.py
@@ -6,8 +6,33 @@ class Node:
         self.name = ""
         self.description = ""
         self.activations = []
+        self.type = ""
+        self.children = []
         # TODO: add the different modality times
         self.time = 0.0
         self.time_hist = []
+        self.new_time = 0.0  # this is for activations that have not yet been resolved?
         self.completed = None
         self.deadline = None
+
+        self.activation_applied = False  # this is used by Graph to determine if it has grabbed time from all of its children yet or not
+
+    def add_activation(self, node, weight):
+        """ Adds a connection FROM this node TO the passed node with given weight in [0, 1] """
+
+        self.activations.append((node, weight))
+        node.children.append(self)
+
+    def get_connection_weight(self, node):
+        """ Return the weight of the connection between this node TO the node passed """
+        for activation in self.activations:
+            if activation[0] == node:
+                return activation[1]
+
+    def activate(self):
+        """ Update new_time from children's new_time. (Propagate's up from leaves) Pseudo-recursive """
+        if not self.activation_applied:
+            for child in self.children:
+                child.activate()
+                self.new_time += child.new_time * child.get_connection_weight(self)
+            self.activation_applied = True

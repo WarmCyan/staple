b06fa740d5af44e898c14d0b9cbaa46a514b75da
Nathan Martindale <nathanamartindale@gmail.com>
2022-11-06 22:38:13 -0500

    Fancier rendering

--- staple/rendering.py
+++ staple/rendering.py
@@ -1,6 +1,8 @@
 import staple.graph
 import staple.util
 
+import datetime
+
 
 def get_graphviz_rep(graph):
     """ Return a text version of the graph in graphviz syntax """
@@ -10,13 +12,16 @@ def get_graphviz_rep(graph):
     graphviz_string = "digraph G {"
 
     for node in graph.nodes:
-        graphviz_string += f"\n{node.name}"
+        #graphviz_string += f"\n{node.name} [label='" + "{ " + node.name + " | " + node.type + " | " + str(node.time) + " }]"
+        graphviz_string += f"\n{node.name} [label=" + "\"" + node.name + " | " + node.type +  " | " + str(node.time) + "\"]"
+        #graphviz_string += f"\n{node.name} [label=< <table><tr>{node.name}</tr><tr>{str(node.time)}</tr></table> >]"
 
     for node in graph.nodes:
         for activation in node.activations:
             graphviz_string += f"\n{activation[0].name} -> {node.name} [label=\"{activation[1]}\"]"
 
     graphviz_string += "\n}"
+    print(graphviz_string)
     return graphviz_string
 
 
@@ -34,5 +39,9 @@ def render_text_graph(graphviz_string):
 
 
 
-def render_week_plan(graph, plan):
-    pass
+def render_plan(graph, plan):
+    render = ""
+
+    render += plan.start_date.strftime("%y-%m-%d")
+
+    return render
